758e9f57cfbb8b29108cc5d04ead25010b4d95c1
rrgayhart <rrgayhart@gmail.com>
2014-06-01 10:55:55 -0600

    completes challenge 1

--- basics/npm/app.js
+++ basics/npm/app.js
@@ -3,7 +3,7 @@ var twilio = require('twilio');
 var client = twilio();
 
 client.sendMessage({
-  to: '+17174440868',
+  to: '+1717440868',
   from: '+18016917578',
   body: 'hello world'
 }, function(err, message) {
