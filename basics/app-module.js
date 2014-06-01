d54a17fc9f50a9b07f0863fbd3723d5e69408536
rrgayhart <rrgayhart@gmail.com>
2014-05-31 18:46:39 -0600

    CommonJS lesson

---
+++ basics/app-module.js
@@ -0,0 +1,9 @@
+var logger = require('./logger');
+var Point = require('./Point');
+
+logger.info('hello world');
+
+var pt = new Point(50,60);
+
+pt.print();
+
---
+++ basics/logger.js
@@ -0,0 +1,7 @@
+exports.info = function(msg) {
+  console.log(new Date()+':'+msg);
+};
+
+exports.error = function(msg) {
+  console.error(msg);
+};
